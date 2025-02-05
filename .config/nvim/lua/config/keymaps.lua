da36c8e59d6108f29531d8eb30327f1e52a526fd
AntonWangDTU <s183220@dtu.dk>
2025-02-05 17:43:26 +0100

    nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -6,16 +6,39 @@
 -- Keybinding setup for LuaSnip
 local ls = require("luasnip")
 
--- Keybinding for jumping to the next snippet placeholder
-vim.keymap.set({ "i", "s" }, "<C-k>", function()
-  if ls.jumpable(1) then
-    ls.jump(1)
-  end
-end, { silent = true })
+--  -- Keybinding for jumping to the next snippet placeholder
+--  vim.keymap.set({ "i", "s" }, "<C-k>", function()
+--    if ls.jumpable(1) then
+--      ls.jump(1)
+--    end
+--  end, { silent = true })
+--
+--  -- Keybinding for jumping back to the previous snippet placeholder
+--  vim.keymap.set({ "i", "s" }, "<C-j>", function()
+--    if ls.jumpable(-1) then
+--      ls.jump(-1)
+--    end
+--  end, { silent = true })
+--
+--  -- keymaps for changing snippet
+--  vim.keymap.set({ "i" }, "<C-K>", function()
+--    ls.expand()
+--  end, { silent = true })
+--  vim.keymap.set({ "i", "s" }, "<C-L>", function()
+--    ls.jump(1)
+--  end, { silent = true })
+--  vim.keymap.set({ "i", "s" }, "<C-J>", function()
+--    ls.jump(-1)
+--  end, { silent = true })
+--
+--  vim.keymap.set({ "i", "s" }, "<C-E>", function()
+--    if ls.choice_active() then
+--      ls.change_choice(1)
+--    end
+--  end, { silent = true })
 
--- Keybinding for jumping back to the previous snippet placeholder
-vim.keymap.set({ "i", "s" }, "<C-j>", function()
-  if ls.jumpable(-1) then
-    ls.jump(-1)
-  end
-end, { silent = true })
+-- navigate within insert mode (with remaps)
+vim.keymap.set("i", "<c-h>", "<Left>", { remap = true })
+vim.keymap.set("i", "<c-l>", "<Right>", { remap = true })
+vim.keymap.set("i", "<c-j>", "<Down>", { remap = true })
+vim.keymap.set("i", "<c-k>", "<Up>", { remap = true })
