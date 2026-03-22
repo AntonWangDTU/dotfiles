3fdbbe8bd41c3caac6742d8ae8505c83b8806283
AntonWangDTU <s183220@dtu.dk>
2026-03-22 20:27:57 +0100

    new

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,19 +2,18 @@ return {
   -- Color schemes
   { "ellisonleao/gruvbox.nvim" },
   { "rebelot/kanagawa.nvim" },
-  { "catppuccin/nvim", name = "catppuccin", lazy = false },
+  { "catppuccin/nvim", name = "catppuccin", lazy = false, priority = 1000 },
   { "rose-pine/neovim", name = "rose-pine" },
   { "sainnhe/everforest" },
   { "bluz71/vim-nightfly-colors" },
   { "NLKNguyen/papercolor-theme" },
   { "romainl/Apprentice" },
   { "Mofiqul/vscode.nvim" },
-
   -- Configure LazyVim to load colorscheme
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-macchiato", -- Set your desired default colorscheme here
+      colorscheme = "catppuccin-macchiato",
     },
   },
 }
